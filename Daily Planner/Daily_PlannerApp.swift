8d36a0c4b9cd871413d0b8a11acb48f381d937ef
mayur7267 <mayurlate.7267@gmail.com>
2025-09-05 16:12:08 +0530

    Initial Commit

---
+++ Daily Planner/Daily_PlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Daily_PlannerApp.swift
+//  Daily Planner
+//
+//  Created by Mayur on 05/09/25.
+//
+
+import SwiftUI
+
+@main
+struct Daily_PlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Daily PlannerTests/Daily_PlannerTests.swift
@@ -0,0 +1,17 @@
+//
+//  Daily_PlannerTests.swift
+//  Daily PlannerTests
+//
+//  Created by Mayur on 05/09/25.
+//
+
+import Testing
+@testable import Daily_Planner
+
+struct Daily_PlannerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
